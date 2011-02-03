b69eb55883075e3a31e3cdfed3e8a3d1d56a84ec
Watermelon Express <developer@watermelonexpress.com>
2011-02-03 17:18:48 -0600

    fixed rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ default_run_options[:pty] = true
 ssh_options[:keys] = [File.join(ENV['HOME'],'.ssh','id_rsa')]
 
 set :deploy_to, "/var/www/rails_apps/#{ application }"
-set :deploy_via
+
 
 
 namespace :deploy do
